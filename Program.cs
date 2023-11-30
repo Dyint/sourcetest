0250863874797417468933d4bebba191d16cb1c9
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2023-11-30 11:28:18 +0900

    commit Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+
+
+public static class Program {
+  public static void main(){
+    Console.WriteLine("hi");
+    string input = Console.ReadLine();
+    input.split();
+
+}
+
+}
